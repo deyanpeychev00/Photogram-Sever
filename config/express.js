612b3162596a5f09d8d2614cb74dc3c96f166a41
Deyan Peychev <deyan.p.peychev@gmail.com>
2018-08-06 15:15:06 +0300

    Updated CORS

--- config/express.js
+++ config/express.js
@@ -14,6 +14,13 @@ module.exports = (app, config) => {
     //create a cors middleware
     app.use(cors());
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+
     // This makes the content in the "public" folder accessible for every user.
     app.use(express.static(path.join(config.rootFolder, 'public')));
 };
